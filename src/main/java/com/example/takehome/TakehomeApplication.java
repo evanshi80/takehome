7746f1fa283ee2dc87121bd810a6ea02c350548c
evanshi80 <yufanshi@me.com>
2023-01-17 21:48:41 -0500

    Test Nacos integration

--- src/main/java/com/example/takehome/TakehomeApplication.java
+++ src/main/java/com/example/takehome/TakehomeApplication.java
@@ -1,13 +1,14 @@
 package com.example.takehome;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
+@NacosPropertySource(dataId = "example", autoRefreshed = true)
 public class TakehomeApplication {
-
 
     public static void main(String[] args) {
         log.debug("This is a debug message");
--- src/main/java/com/example/takehome/controller/DefaultRestController.java
+++ src/main/java/com/example/takehome/controller/DefaultRestController.java
@@ -1,7 +1,9 @@
 package com.example.takehome.controller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.example.takehome.domain.OtherCountriesResponse;
 import com.example.takehome.service.CountryDataService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,11 @@ import java.util.List;
 @RestController
 public class DefaultRestController {
 
+
+    // just to test the Nacos Config Service
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
     private CountryDataService countryDataService;
 
     public DefaultRestController(CountryDataService countryDataService) {
@@ -20,6 +27,7 @@ public class DefaultRestController {
 
     @PostMapping("findOtherCountries")
     public Collection<OtherCountriesResponse> findOtherCountries(@RequestBody List<String> countryCodes) {
+        System.out.println("useLocalCache: " + useLocalCache);
         return countryDataService.findOtherCountries(countryCodes);
     }
 }
